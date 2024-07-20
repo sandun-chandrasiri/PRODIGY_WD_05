37354788abe26986d23613695a6a8c9c115578a3
Sandun Chandrasiri <131155243+sandun-chandrasiri@users.noreply.github.com>
2024-07-20 05:54:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "";
+
+weatherForm.addEventListener("submit", async event => {
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+}
